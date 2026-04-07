88e1661857b8710aacf4d28f1ec6edb5a347cd22
Jody Steele <steelej@queensu.ca>
2026-04-07 19:20:43 -0400

    Add fetching of content through valence

--- api_le_content.go
+++ api_le_content.go
@@ -1,6 +1,9 @@
 package valence
 
-import "net/url"
+import (
+	"io"
+	"net/url"
+)
 
 // GetTableOfContents returns the full content table of contents for an org unit.
 // GET /d2l/api/le/{leVersion}/{orgUnitId}/content/toc
@@ -8,4 +11,10 @@ func (c *Client) GetTableOfContents(orgUnitId int64, params url.Values) (*TableO
 	var out TableOfContents
 	err := c.get(c.lePath("%d/content/toc", orgUnitId), params, &out)
 	return &out, err
+}
+
+// GetContentTopicFile returns the raw bytes of a content file topic.
+// GET /d2l/api/le/{leVersion}/{orgUnitId}/content/topics/{topicId}/file
+func (c *Client) GetContentTopicFile(orgUnitId, topicId int64) (io.ReadCloser, error) {
+	return c.getRaw(c.lePath("%d/content/topics/%d/file", orgUnitId, topicId), nil)
 }
